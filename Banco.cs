9ee70afceefa6188fe865ec272f4417ae33922a4
wineezidio <127625157+wineezidio@users.noreply.github.com>
2025-01-09 02:29:56 -0300

    Add files via upload

---
+++ Banco.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ConsoleApp3
+{
+    internal class Banco
+    {
+        public double Saldo { get; private set; } // proprities onde eu posso imprimir o saldo sem poder alterar ele
+        public string Titular { get; set; } // aqui eu posso alterar e chamar o Titular
+        public int Conta { get; private set; } // aqui eu nao posso alterar o numero da conta
+
+
+        public Banco(int conta, string titular) /* construtor com 2 paramtros int e string e
+            instaciando os atributos nos parametros */
+        {
+            Titular = titular;
+            Conta = conta;
+            
+        }
+
+        public Banco(int conta, string titular, double saldo) : this (conta, titular) /*construtor com 3 paramtros int, string e double 
+            usando o this para chamar os parametros e os atributos do outro construtor e passando tambem um parametro para o atributo instaciado  */
+        {
+            Saldo = saldo;
+        }
+
+        public void  Deposito(double depositofinal) /* Aqui sao os metodos de operacao "esse double depositofinal" 
+             vai me trazer um valor da outra classe 
+            para somar com o saldo e na outra classe eu preciso chamar esse metodo com o nome 
+            atribuido a ele la "banco.Deposito("aqui minha variavel double") "*/
+        {
+            Saldo = Saldo + depositofinal;
+        }
+
+        public void Saque(double depositofinal)
+        {
+            Saldo -= depositofinal + 5.0;
+        }
+
+
+
+
+        public override string ToString()
+        {
+            return "Conta " +
+                Conta + 
+                ", Titular: " 
+                + Titular 
+                + ", Saldo: $ " 
+                + Saldo.ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using ConsoleApp3;
+
+namespace Console3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco banco;
+
+            Console.Write("Entre com o numero da conta: ");
+            int conta = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o nome do titular: ");
+            string titular = Console.ReadLine();
+            Console.Write("havera deposito inicial (s/n) ? ");
+            char depositoInicial = char.Parse(Console.ReadLine());
+            if (depositoInicial == 's')
+            {
+                Console.Write("Entre com o valor do deposito: ");
+                double depositoI = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                banco = new Banco(conta, titular, depositoI); /* Aqui precisei criar uma variavel para informar o valor do deposito
+                               e depois informei ela dentro do construtor para guardar o valor atribuido nela */
+            } else
+            {
+                 banco = new Banco(conta, titular); /* se eu nao fizer o deposito o else ira usar o construtor para 
+                para imprimir o numero int da conta e a nome string do titular */
+            }
+
+           
+
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(banco);
+            Console.WriteLine();
+
+            Console.Write("Entre com um valor para o deposito: ");
+            double depositofinal = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            banco.Deposito(depositofinal); // Nesse caso o metodo criado na classe foi chamado na qual é " banco.Deposito" e no (depositofinal vai jogar o valor da variavel dentro da operacao)
+            //e ja deixa salvo o valor devido o "Saldo" estar dentro do metodo
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(banco);
+            Console.WriteLine();
+
+            Console.Write("Entre com um valor para saque: ");
+            depositofinal = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Dados da conta atualizados: ");
+            banco.Saque(depositofinal);
+            Console.WriteLine(banco);
+
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
